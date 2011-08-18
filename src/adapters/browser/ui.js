83ea09223f7847c9929d6110af9cea4f8538411c
pbraden <peterbraden@peterbraden.co.uk>
2011-08-18 15:32:27 -0700

    by default, don't show failures in test runner

--- src/adapters/browser/ui.js
+++ src/adapters/browser/ui.js
@@ -3,7 +3,9 @@ if (typeof foounit.ui == 'undefined'){
 }
 
 (function (ui){
-  var _body, _index = 0, _autoScrolling = true;
+  var _body, _index = 0, _autoScrolling = true, errors = []
+    , logAll = window.location.hash.indexOf("all") > -1
+  
 
   var _createTitleNode = function (title, index, className){
     var titleDiv = document.createElement('div');
@@ -136,6 +138,7 @@ if (typeof foounit.ui == 'undefined'){
       _progressBar.fail(_index);
       _scroller.bottom();
       ++_index;
+      errors.push(example);
     } catch (e){
       alert('foounit.ui.onFailure: ' + e.message);
     }
@@ -144,9 +147,11 @@ if (typeof foounit.ui == 'undefined'){
   /**
    * Called when the runner runs an example that succeeds
    */
-  ui.onSuccess = function (example){
+  ui.onSuccess = function (example){    
     try {
-      _body.appendChild(_createSuccessNode(example, _index));
+      if (logAll){
+        _body.appendChild(_createSuccessNode(example, _index));
+      }  
       _progressBar.success(_index);
       _scroller.bottom();
       ++_index;
